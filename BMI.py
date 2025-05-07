07b7da50263989e6600a31d9da6c81e7e7f2f74d
ameer <ameersayf.24@ichat.sp.edu.sg>
2025-05-07 11:37:37 +0800

    final

--- BMI.py
+++ BMI.py
@@ -1,4 +1,32 @@
+print("hello world")
+
 def calculate_bmi(height, weight):
-      print("Height = " + height)
-      print("Weight = " + weight)
-      calculate_bmi(weight=57, height=1.73)
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/height*height
+    print(bmi)
+
+calculate_bmi(weight=57, height=1.73)
+
+def display_main_menu():
+    print("display_main_menu")
+
+
+def get_user_input():
+    print("get_user_input")
+
+
+def calc_average(temp_list):
+    print("calc_average")
+
+
+def find_min_max(temp_list):
+    print("find_min_max")
+
+
+def sort_temperature(temp_list):
+    print("sort_temperature")
+
+
+def calc_median_temperature(temp_list):
+    print("calc_median_temperature")
